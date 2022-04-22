d6770ed773e0526bc828aa38ee15f6d44515f00e
Wilssonmartee <wilssonmartee@nutyx.localdomain>
2022-04-21 21:23:38 -0400

    fix minor issues

--- install_caurds.sh
+++ install_caurds.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+
+if ! [ $(id -u) = 0 ]; then
+   echo "ERROR! Use a root account to install Caurds." >&2
+   exit 1
+fi
+
+
 cards_config=$(cards config)
 
 if [[ $cards_config == *"testing/base"* ]] ; then
@@ -8,6 +16,9 @@ else
   nutyx_version="rolling"
 fi
 
+cards sync && cards upgrade
+(pkginfo -i | grep " git ") || cards install git
+
 cd /tmp
 
 curl -O https://raw.githubusercontent.com/wilssonmartee/caurds/main/cards.conf
@@ -15,20 +26,19 @@ curl -O https://raw.githubusercontent.com/wilssonmartee/caurds/main/pkgmk.conf
 
 sed -i 's/rolling/'${nutyx_version}'/g' /tmp/cards.conf
 
-cp -f --backup /tmp/cards.conf /etc/cards.conf
-cp -f --backup /tmp/pkgmk.conf /etc/pkgmk.conf
+mv -f /etc/cards.conf /etc/cards.conf.backup
+mv -f /etc/pkgmk.conf /etc/pkgmk.conf.backup
+cp -f /tmp/cards.conf /etc/cards.conf
+cp -f /tmp/pkgmk.conf /etc/pkgmk.conf
 
-cards sync && cards upgrade
-(pkginfo -i | grep " git ") || cards install git
 
 mkdir -p /usr/share/ports/xposed/caurds
 cd /usr/share/ports/xposed/caurds
 curl -O https://raw.githubusercontent.com/wilssonmartee/nutyx-xposed-pkg-x86_64/main/caurds/Pkgfile
 
 pkgmk -d
+cards sync && cards upgrade
 cards install caurds
-caurds sync
-caurds upgrade
-
-echo "***** Caurds installed!"
+caurds sync && echo -e "\n\n***** Caurds installed!\n\n"
+caurds --help
 
